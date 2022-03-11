72916ed7b7ffe14408771977d3ea9e78af72aa8b
AlekVojceska <98151005+AlekVojceska@users.noreply.github.com>
2022-03-11 10:37:04 +0100

    esercizio1 vettori numeri

---
+++ VojceskaVettoriNumero/VojceskaVettoriNumero/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VojceskaVettoriNumero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            const int dimensione = 400;
+            int i = 0;
+            Random generatore = new Random();
+            for (int c = 0; c < dimensione; c++)
+            {
+                int numerocasuale = generatore.Next(0, 100);
+                Console.WriteLine($"{numerocasuale}");
+            }
+            
+        }
+    }
+}
